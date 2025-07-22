cb89a7297760d40da18f9172180b220144f0e7af
Soesep Dev <soesep.dev@gmail.com>
2025-07-22 14:19:01 +0700

    chore: initial commit

---
+++ src/config/db.ts
@@ -0,0 +1,9 @@
+import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pgp = pgPromise();
+const db = pgp(process.env.DATABASE_URL as string);
+
+export default db;
--- src/modules/test/models/test.ts
+++ src/modules/test/models/test.ts
@@ -1,5 +1,8 @@
+import db from '../../../config/db';
+
 export const getTest = async () => {
+  const { result } = await db.one(`SELECT 'DB OK' AS result`);
   return {
-    'message':'test'
-  }
+    message: result
+  };
 };
